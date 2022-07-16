63f440e20f7cf3a161afb16d177503b0fe9b2e16
Abdulramon Jemil <jameeltemidayo@gmail.com>
2022-07-16 16:50:22 +0100

    Removed the custom loading solution

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /**
  * This file is meant to meant to be for testing out random code
  * The files for the project are located in the js folder
- * Check the comment at the bottom of the script tag in the index.html file to learn more
  */
 
 
 console.log("Run your random code here")
+
